88f0f7913fee2a7239dc23d739fec78f1358d2e7
Tran Van <vanbaotran201194@gmail.com>
2021-06-05 13:23:00 +0200

    adding my orders

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -18,12 +18,16 @@ router.get('/:id/addtobag',(req,res,next)=>{
     res.redirect(`/products`)
     
 }) 
-//BONUS: POP-UP 'ADDED TO BAG'/CONTINUE SHOPPING/CHECK OUT
+//BONUS: POP-UP 'ADDED TO BAG'/CONTINUE SHOPPING/CHECK OUT--> use flash
 
 /////BUY NOW/////////
 router.get('/:id/orders',(req,res,next)=>{
+    //make sure the client log in before placing an order
+    if (!req.session.currentUser){
+        res.render('auth/login',{errorMessage:'Please login to place an order! If you are not part of the family yet, please sign up'})
+    }
     //if there is no product in shopping cart, then add to cart then check out 
-    if (!req.session.cart){
+    else if (!req.session.cart){
         req.session.cart = [req.params.id]
         Product.findById(req.params.id)
         .then(productFromDB=>{
@@ -33,12 +37,9 @@ router.get('/:id/orders',(req,res,next)=>{
                 total: productFromDB.price ,
             })
             .then(order=>{
-                console.log('shopping cart',req.session.cart)
-                console.log('req.session',req.session)
-                console.log('order newly created',order)
                 req.session.order = order;
                 console.log('session.order:',req.session.order)
-                res.render('orders/shopping-cart',{theOrder :order})
+                res.redirect('shoppingcart')
             })
             .catch(err=>next(err))
         })
@@ -48,41 +49,24 @@ router.get('/:id/orders',(req,res,next)=>{
         req.session.cart.push(req.params.id) 
         let productsArray = req.session.cart.filter((el)=>req.session.cart.indexOf(el)===req.session.cart.lastIndexOf(el))
         console.log('products Array', productsArray)
-        //1. create an order with the last item (:id) in shopping cart
-        Product.findById(req.params.id)
-        .then(productFromDB=>{
-            console.log('req.session',req.session)
+        let promises = [];
+        productsArray.forEach(id=>promises.push(Product.findById(id)))   
+        Promise.all(promises).then(products=>{
+            let priceArray = products.map(el=>el.price)
+            let total = priceArray.reduce((acc,el)=>acc+el)
             Order.create({
-                productId: productFromDB._id,
-                userId: req.session.currentUser,
-                total: productFromDB.price ,
+                productId: productsArray,
+                userId: req.session.currentUser._id, 
+                total: total,
             })
-            .then(orderFromDB=>{
-                let total = 0;
-        //2. Running a loop to calculate the total
-                for (let i=0; i<productsArray.length;i++){
-                    Product.findById(productsArray[i])
-                    .then(productFromDB=>{
-                        total+=productFromDB.price
-                        console.log('total',total) 
-                    })    
-                    .catch(err=>next(err))
-                }
-                console.log('total',total)  
-                orderFromDB.productId = productsArray;
-                orderFromDB.total = total;
-        //3. Save the update of the order
-                orderFromDB.save()
-                .then(orderFromDB=>{
-                    console.log('order created',orderFromDB)
-                    req.session.order = orderFromDB;
-                    res.render('orders/shopping-cart',{theOrder: orderFromDB})
-                })
-                .catch(err=>next(err))
+            .then(order=>{
+                console.log('order just created',order)
+                res.redirect('shoppingcart')
+                req.session.order = order;
+             
             })
-            .catch(err=>next(err))
         })
-        .catch(err=>console.log('error when creating order',err))            
+        .catch(err=>console.log('error when calculation total',err))
     }
 })
 router.get('/shoppingCart',(req,res,next)=>{
@@ -93,7 +77,17 @@ router.get('/shoppingCart',(req,res,next)=>{
     })
     .catch(err=>next(err))
 })
+router.get('/myorders',(req,res,next)=>{
+    User.findById(req.session.currentUser)
+    .then(()=>{
+        Order.find()
+        .then(orders=>{
+            console.log('myorders', orders)
+            res.render('orders/myorders',{myOrders: orders})
+        })
+    })
+        
     
-
+})
 
 module.exports = router;
