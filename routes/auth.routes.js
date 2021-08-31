c3f10e62c95ee6ca6dcf1e7fe67c9957e259997a
Văn Bảo Trân <vanbaotran201194@gmail.com>
2021-08-31 15:03:36 +0200

    CSS review - responsive

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -181,9 +181,29 @@ router.get('/wishlist',(req,res,next)=>{
     res.redirect('/login')
   }
 })
-
-
-
+/////SEND EMAIL///////
+router.get('/send-email',(req,res,next)=>{
+  res.render('auth/nodemailer',{})
+})
+router.post('/send-email',(req,res,next)=>{
+  let {email, subject, message}=req.body;
+   let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'vanbaotran201194@gmail.com',
+      pass: process.env.PASSWORD
+    }
+  });
+  transporter.sendMail({
+    from: '"My Awesome Project " <myawesome@project.com>',
+    to: email, 
+    subject: subject, 
+    text: message,
+    html: `<b>${message}</b>`
+  })
+  .then(info => res.render('message', {email, subject, message, info}))
+  .catch(error => console.log(error));
+});
 
 
 router.get('/wishlist', (req, res) => res.render('users/wishlist'));
